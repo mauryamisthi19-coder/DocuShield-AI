102c5585496b5a77cac097e4298bb6d9d512d9b9
Misthi Maurya <maurya.misthi19@gmail.com>
2026-07-25 22:31:55 +0530

    Initial Spring Boot backend setup

---
+++ backend/src/test/java/com/docushield/DocushieldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docushield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocushieldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
